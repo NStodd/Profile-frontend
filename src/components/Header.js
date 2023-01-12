598c1f2362d9b3b36730938456ac1197fa46859c
Nate Stodd <nathan.stoddard@gmail.com>
2023-01-11 21:45:15 -0500

    frontend complete

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+function Header (props) {
+    const navStyle = {
+        display: "flex",
+        justifyContet: "space-around",
+        border: "3px solid black",
+        padding: "8px",
+        width: "90%",
+        margin: "auto",
+    }
+
+    return (
+        <header>
+            <h1>My Portfolio Page</h1>
+            <nav style={navStyle}>
+                <Link to="/"><div>HOME</div></Link>
+                <Link to="/about"><div>ABOUT</div></Link>
+                <Link to="/projects"><div>PROJECTS</div></Link>
+            </nav>
+        </header>
+    )
+
+}
+
+export default Header
